6d987960bb409f76277340a9f122ba13adad64ad
thomas5476 <thomas.bamberger.070605@gmail.com>
2024-06-18 08:21:10 +0200

    10th commit

--- src/test/Hallo.java
+++ src/test/Hallo.java
@@ -5,11 +5,9 @@ public class Hallo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println("Hallo Hallein");
-=======
-		System.out.println("Hallo HTL Hallein");
->>>>>>> branch 'master' of https://github.com/thomas5476/Example
+
 	}
 
 }
