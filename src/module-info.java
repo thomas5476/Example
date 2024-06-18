c9db30279a79ab845ff139dd6fa71927856a95fa
thomas5476 <thomas.bamberger.070605@gmail.com>
2024-06-18 08:08:04 +0200

    5 Commit

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,5 @@
 /**
-<<<<<<< HEAD
- * Hallo HAK Hallein
-=======
- * Hallo HTL Salzburg
->>>>>>> branch 'master' of https://github.com/thomas5476/Example
+ * Hallo HTL Hallein
  */
 /**
  * @author thoma
