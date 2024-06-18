24d2aea2cfca24628842053cf8e9d4cbf8ba9fee
thomas5476 <thomas.bamberger.070605@gmail.com>
2024-06-18 08:40:08 +0200

    13th commit

--- src/test/Hallo.java
+++ src/test/Hallo.java
@@ -6,7 +6,7 @@ public class Hallo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hallo Hallein");
+		System.out.println("Hallo HTL Hallein");
 	}
 
 }
